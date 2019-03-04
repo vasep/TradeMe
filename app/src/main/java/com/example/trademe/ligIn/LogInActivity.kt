5277db4d9daa0713bdb6c57e7294e2f142398c87
vasep <46342415+vasep@users.noreply.github.com>
2019-03-04 15:55:59 +0100

    logging in working correctly

--- app/src/main/java/com/example/trademe/ligIn/LogInActivity.kt
+++ app/src/main/java/com/example/trademe/ligIn/LogInActivity.kt
@@ -3,24 +3,29 @@ package com.example.trademe.ligIn
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.trademe.R
 import com.example.trademe.mvp.BaseActivity
+import kotlinx.android.synthetic.main.login_main.*
 
 
 class LogInActivity : BaseActivity<LogInContract.View, LogInPresenter>(), LogInContract.View {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_main)
 
+        login_button.setOnClickListener {
+            presenter.onLoginButtonPressed(edt_email.text.toString(), edt_password.text.toString())
+        }
     }
 
     override fun setProgressBar(show: Boolean) {
     }
 
-    override fun onLoginResult(result: String) {
-
+    override fun onLoginResult(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
