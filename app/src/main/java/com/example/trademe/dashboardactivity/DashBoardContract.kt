f6e763f38c881cc9028b0b9b2c089636d270103c
vasep <46342415+vasep@users.noreply.github.com>
2019-03-14 16:09:38 +0100

    logging in working correctly

--- app/src/main/java/com/example/trademe/dashboardactivity/DashBoardContract.kt
+++ app/src/main/java/com/example/trademe/dashboardactivity/DashBoardContract.kt
@@ -1,9 +1,30 @@
 package com.example.trademe.dashboardactivity
-
+import com.example.trademe.models.Posts
 import com.example.trademe.mvp.IBaseView
 
 class DashBoardContract {
 
     interface View : IBaseView {
+        fun navigateToSettings()
+
+        fun navigateToAboutScreen()
+
+        fun navigateToEditProfile()
+
+        fun onEmaptyDashboardError()
+
+        fun loadDashboard(postsDashBoard : ArrayList<Posts>)
+
+        fun navigateToPost(post : Posts)
+
+        fun handleEmptyView()
     }
+
+    interface Presenter {
+
+        fun onPostClicked(post: Posts)
+
+        fun onNavigationItemSelected(navItemSpecifier: Int)
+    }
+
 }
